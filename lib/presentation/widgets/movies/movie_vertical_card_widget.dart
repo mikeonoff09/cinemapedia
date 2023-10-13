ef8d2a97e7d143fb6b83f567954f8d9baa1e0234
Miguel Lara <miguel.larasj@gmail.com>
2023-10-13 12:10:55 -0500

    movie card widget layout impruvements

--- lib/presentation/widgets/movies/movie_vertical_card_widget.dart
+++ lib/presentation/widgets/movies/movie_vertical_card_widget.dart
@@ -13,36 +13,36 @@ class MovieVerticalCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      width: 180,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            padding: const EdgeInsets.all(8),
-            height: 250,
-            width: 180,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: Image.network(
-                movie.posterPath,
-                fit: BoxFit.cover,
-                loadingBuilder: (context, child, loadingProgress) {
-                  if (loadingProgress != null) {
-                    return const DecoratedBox(
-                      decoration: BoxDecoration(
-                        color: Colors.black12,
-                      ),
-                    );
-                  }
-                  return FadeIn(child: child);
-                },
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        width: 180,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 240,
+              width: 170,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20),
+                child: Image.network(
+                  movie.posterPath,
+                  fit: BoxFit.cover,
+                  loadingBuilder: (context, child, loadingProgress) {
+                    if (loadingProgress != null) {
+                      return const DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: Colors.black12,
+                        ),
+                      );
+                    }
+                    return FadeIn(child: child);
+                  },
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child: Text(
+            const SizedBox(height: 8),
+            Text(
               movie.title,
               maxLines: 3,
               style: const TextStyle(
@@ -50,28 +50,28 @@ class MovieVerticalCardWidget extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-          ),
-          Row(
-            children: [
-              Icon(
-                Icons.star_half,
-                color: Colors.amber[400],
-              ),
-              Text(
-                movie.voteAverage.toString(),
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
+            Row(
+              children: [
+                Icon(
+                  Icons.star_half,
                   color: Colors.amber[400],
                 ),
-              ),
-              const SizedBox(width: 8),
-              Text(NumberFormat.compactCurrency(
-                decimalDigits: 2,
-                symbol: '',
-              ).format(movie.voteCount))
-            ],
-          )
-        ],
+                Text(
+                  movie.voteAverage.toString(),
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.amber[400],
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Text(NumberFormat.compactCurrency(
+                  decimalDigits: 2,
+                  symbol: '',
+                ).format(movie.voteCount))
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
